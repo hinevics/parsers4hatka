44ae644e5a24a790fa792da7eae56994c9998d3a
Sasha Khinevich <hinevics@gmail.com>
2023-10-20 00:00:32 +0300

    add module for css selectors

---
+++ dagster-project/pages_parser/realt/css_selectors.py
@@ -0,0 +1,6 @@
+ADRESS = r'li.md\:w-auto'
+DESC_NOTE = r'section.bg-white:nth-child(6) > div:nth-child(2)'
+PRICE = r'.md\:items-center > div:nth-child(1) > h2:nth-child(1)'
+UPDATE_DATE = r'div.text-caption:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)'
+CHAR = r'ul.w-full:nth-child(2)'
+NUMBER_FLOOR = r'div.last\:mr-0:nth-child(3) > div:nth-child(1)'
