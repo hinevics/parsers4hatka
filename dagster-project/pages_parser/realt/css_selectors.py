b4fe143cc9752ed6a633d42db923251acc78d61c
Sasha Khinevich <hinevics@gmail.com>
2023-10-20 18:48:55 +0300

    add new seletor for parsing

--- dagster-project/pages_parser/realt/css_selectors.py
+++ dagster-project/pages_parser/realt/css_selectors.py
@@ -1,6 +1,6 @@
 ADRESS = r'li.md\:w-auto'
 DESC_NOTE = r'section.bg-white:nth-child(6)'
-PRICE = r'.md\:items-center > div:nth-child(1) > h2:nth-child(1)'
+PRICE = r'#__next > div.styles_layout__ay89B > div.styles_layout__top__839os > main > div > div > div > div.md\:pb-11 > div.-mx-1\.5.sm\:-mx-2\.5.md\:-mx-3\.75.flex.flex-wrap.mt-0 > div.lg\:w-4\/12.lg\:block.hidden.w-full.p-1\.5.sm\:p-2\.5.lg\:p-3\.75.lg\:py-4 > div > div.bg-white.flex.flex-wrap.md\:p-6.rounded-md > div.md\:w-full.text-info-500.md\:items-center.relative.flex.flex-wrap.w-1\/2.mb-6 > div.mt-0 > div > h2'
 UPDATE_DATE = r'div.text-caption:nth-child(2) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1) > span:nth-child(1)'
 CHAR = r'ul.w-full:nth-child(2)'
 NUMBER_FLOOR = r'div.last\:mr-0:nth-child(3) > div:nth-child(1)'
